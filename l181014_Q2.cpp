5563d3b0da3c24684e2dd976a6e48db8da54d9f5
181014muzamil <46879735+181014muzamil@users.noreply.github.com>
2019-02-04 22:00:58 +0500

    sir i have not made this function earlier but for lab 3 i have made this function because i have to also use it in lab 3 so please grant me marks for this function

--- l181014_Q2.cpp
+++ l181014_Q2.cpp
@@ -1,35 +1,71 @@
 #include<iostream>
-#include<string>
 #include<fstream>
+#include<string>
 using namespace std;
 
-struct gitHubUser{
-	//struct of github users
-	string name;
+struct gitHubUser{  //struct of github users
+
+	string firstName;
 	string userName;
+	string password;
 	string email;
-	int folderCount;
-	string folderName;
+	string * folders;
+        int    folderCount; 
+        string * nameofins;
+	string * qualification;  
 };
 
-void readDataFromFile( gitHubUser * users, string filepath)
-{  //to read data from file
-	ifstream fin;
-	ofstream fout;
-	fin.open("filepath");
-	if(fin.fail())
-		cout<<"Could not open file"<<endl;
-	else
+void output(gitHubUser * arr, int userCount)
+{ 
+	//for output the githubusers
+	for(int i=0;i<userCount;i++)
 	{
-	int size;
-	fin>>size;
-	gitHubUser arr[2];
+    cout<<endl;
+    cout<<"the elements of Structs are"<<endl;
+	cout<<"UserName :"<<arr[i].userName<<endl;
+	cout<<"FirstName :"<<arr[i].firstName<<endl;
+	cout<<"Password :"<<arr[i].password<<endl;
+	cout<<"Email :"<<arr[i].email<<endl;
+	cout<<"Folders :"<<arr[i].folderCount<<endl;
+	cout<<endl;
 	}
 }
 
-int main()
+void readDataFromFile( gitHubUser * users)
 {
-	gitHubUsers * a=new gitHubUser[size];
-	readDataFromFile();
+	users=new gitHubUser[2];
+	ifstream fin;
+	fin.open("c:\\temp\\m.txt");
+	if(!fin)
+	{
+		cout<<"Could not load File"<<endl;
+	}
+	else
+	{
+		int size=0;
+		int folder;
+		fin >> size;
+		for(int i=0;i<size;i++)
+		{
+			fin>>users[i].firstName;
+			fin>>users[i].userName;
+			fin>>users[i].email;
+			fin>>users[i].folderCount;
+			folder=users[i].folderCount;
+			users[i].folders=new string[folder];
+			fin.ignore();
+			for(int j=0;j<folder;j++)
+			{
+				getline(fin, users[i].folders[j]);
+			}
+		
+		}
+		output(users,size);
+	}
 }
 
+int main(){
+	gitHubUser * users=new gitHubUser [2];
+        readDataFromFile(users);
+	return 0;
+}
